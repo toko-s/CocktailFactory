e56f085fd63547d471e9314023abdebeeb6f9959
DavitTodua <dtodu18@freeuni.edu.ge>
2021-07-16 16:08:51 +0400

    added Coctail And Ingredients Classes

---
+++ src/main/java/Model/Ingredient.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class Ingredient {
+    private String name;
+    private int weight;
+    private int price;
+
+    public Ingredient(String name, int weight, int price) {
+        this.price = price;
+        this.weight = weight;
+        this.price = price;
+    }
+}
