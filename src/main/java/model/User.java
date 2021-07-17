a627290e9c5914131b99120d6b64461ee2206e47
toko-s <tosula18@freeuni.edu.ge>
2021-07-17 15:00:48 +1200

    Add Cocktail filter and cocktail service

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,14 +1,26 @@
 package model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class User {
     private int id;
     private String username;
     private String password;
+    private List<Cocktail> cocktails;
+
+    public User(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
