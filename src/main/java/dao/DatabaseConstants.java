749fbfe94ce3e5a7b14f23ab3cb7c8facbabf385
canidiesir <mchub18@freeuni.edu.ge>
2021-08-03 00:46:59 +0400

    constants deleted temporarily

--- src/main/java/dao/DatabaseConstants.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-public final class DatabaseConstants {
-    public static final String NAME = "com.mysql.jdbc.Driver";
-    public static final String URL = "jdbc:mysql://localhost:3306/CocktailFactory";
-    public static final String USER = "root";
-    public static final String PASSWORD = "2412";
-}
