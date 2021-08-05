a8beb5280ba54760608d3fd0576d43e011312d9c
canidiesir <mchub18@freeuni.edu.ge>
2021-08-05 21:58:18 +0400

    add cocktail, get user's cocktails

--- src/main/java/servlet/UserServlet.java
+++ src/main/java/servlet/UserServlet.java
@@ -16,7 +16,7 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        CocktailDao dao = new CocktailDao();
+        CocktailDao dao = CocktailDao.getInstance();
         Cocktail cocktail = new Cocktail();
         cocktail.setUserID(1);
         cocktail.setName("Mohito");
